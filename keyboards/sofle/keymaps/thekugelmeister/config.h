ce82314523efc82e39e4dbcc10afc9287bb855d1
Jeremy Marcus <jeremy.i.marcus@gmail.com>
2024-02-13 14:25:55 -0800

    baseline keymap

--- keyboards/sofle/keymaps/thekugelmeister/config.h
+++ keyboards/sofle/keymaps/thekugelmeister/config.h
@@ -4,3 +4,8 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+
+#define ENCODER_DIRECTION_FLIP
+// I don't know why but I can't set this without raising a "redefined" error. Seems that resolution is hard coded in info.json?
+// Instead, I had to duplicate the encoder fields from rev1/info.json, set the resolution there to 4, then delete the resolution settings in rev1/info.json.
+// #define ENCODER_RESOLUTION 1
